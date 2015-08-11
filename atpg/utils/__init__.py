d93fc0fdf4963e2683625eb15774b4df641fefc8
eastzone <eastzonexp@gmail.com>
2015-08-10 22:04:45 -0700

    [license] Removed license from source code

--- atpg/utils/__init__.py
+++ atpg/utils/__init__.py
@@ -1,5 +1,2 @@
 '''
 Created on Aug 14, 2011
-
-@author: peymankazemian
-'''
--- atpg/utils/generate_stanford_one_layer_tf.py
+++ atpg/utils/generate_stanford_one_layer_tf.py
@@ -1,8 +1,2 @@
 '''
 Created on Mar 4, 2012
-
-@author: peymankazemian
-'''
-from utils.load_stanford_backbone import *
-
-generate_stanford_backbne_one_layer_tf()
